e853c7328ad77cbf8382907771e3ff685348c6aa
Alessandro Power <alessandrojpower@gmail.com>
2018-02-07 22:50:29 -0500

    Refactored code to return Response object.

--- httpc/__main__.py
+++ httpc/__main__.py
@@ -4,86 +4,97 @@ from requests import GET, POST
 
 
 def execute_get(args):
-    """"""
-    headers = dict(kv.split(':') for kv in args.headers)
+    """Perform a GET request with the given command line parameters and print
+    the responset to standard out.
+
+    Args:
+        args: Argparse argument object.
+    """
+    headers = dict(kv.split(":") for kv in args.headers)
     response = GET(args.url, headers=headers)
     print(response if args.verbose else response.body)
 
 
 def execute_post(args):
-    """"""
-    # Extract data depending on whether inline-data or file was specified.
+    """Perform a PUT request with the given command line parameters and print
+    the responset to standard out.
+
+    Args:
+        args: Argparse argument object.
+    """
+    # Extract data, either by reading a file or using the inline data.
+    # TODO check if file exists.
     if args.file:
-        with open(args.file, mode='r') as f:
+        with open(args.file, mode="r") as f:
             data = f.read()
     else:
         data = args.inline_data
 
-    headers = dict(kv.split(':') for kv in args.headers)
+    headers = dict(kv.split(":") for kv in args.headers)
     response = POST(args.url, data=data, headers=headers)
     print(response if args.verbose else response.body)
 
 
 parser = argparse.ArgumentParser(
     description=
-    'httpc is a curl-like application but supports HTTP protocol only.',
+    "httpc is a curl-like application but supports HTTP protocol only.",
     add_help=False)
-subparsers = parser.add_subparsers(title='command', metavar='command')
+subparsers = parser.add_subparsers(title="command", metavar="command")
 
 get_parser = subparsers.add_parser(
-    'get',
-    description='Get executes a HTTP GET request for a given URL.',
+    "get",
+    description="Get executes a HTTP GET request for a given URL.",
     add_help=False)
 get_parser.add_argument(
-    '-v',
+    "-v",
     help=
-    'Prints the detail of the response such as protocol, status, and headers.',
-    action='store_true',
-    dest='verbose')
+    "Prints the detail of the response such as protocol, status, and headers.",
+    action="store_true",
+    dest="verbose")
 get_parser.add_argument(
-    '-h',
+    "-h",
     help="Associates headers to HTTP Request with the format 'key:value'.",
-    metavar='key:value',
-    action='append',
+    metavar="key:value",
+    action="append",
     default=[],
-    dest='headers')
-get_parser.add_argument('url', metavar='URL')
+    dest="headers")
+get_parser.add_argument("url", metavar="URL")
 get_parser.set_defaults(func=execute_get)
 
 post_parser = subparsers.add_parser(
-    'post',
-    description='Post executes a HTTP POST request for a given URL and body.',
+    "post",
+    description="Post executes a HTTP POST request for a given URL and body.",
     add_help=False)
 post_parser.add_argument(
-    '-v',
+    "-v",
     help=
-    'Prints the detail of the response such as protocol, status, and headers.',
-    action='store_true',
-    dest='verbose')
+    "Prints the detail of the response such as protocol, status, and headers.",
+    action="store_true",
+    dest="verbose")
 post_parser.add_argument(
-    '-h',
+    "-h",
     help="Associates headers to HTTP Request with the format 'key:value'.",
-    metavar='key:value',
-    action='append',
+    metavar="key:value",
+    action="append",
     default=[],
-    dest='headers')
+    dest="headers")
 data_group = post_parser.add_mutually_exclusive_group(required=True)
 data_group.add_argument(
-    '-d',
-    help='Associates an inline data to the body HTTP POST request',
-    metavar='inline-data',
-    dest='inline_data')
+    "-d",
+    help="Associates an inline data to the body HTTP POST request",
+    metavar="inline-data",
+    dest="inline_data")
 data_group.add_argument(
-    '-f',
-    help='Associates the content of a file to the body HTTP POST request.',
-    metavar='file',
-    dest='file')
-post_parser.add_argument('url', metavar='URL')
+    "-f",
+    help="Associates the content of a file to the body HTTP POST request.",
+    metavar="file",
+    dest="file")
+post_parser.add_argument("url", metavar="URL")
 post_parser.set_defaults(func=execute_post)
 
-help_parser = subparsers.add_parser('help', add_help=False)
+help_parser = subparsers.add_parser("help", add_help=False)
 help_parser.add_argument(
-    'command', choices=('get', 'put'), nargs='?', metavar='command')
+    "command", choices=("get", "put"), nargs="?", metavar="command")
 # help_parser.set_defaults(func=execute_help)
 
 args = parser.parse_args()
--- httpc/requests.py
+++ httpc/requests.py
@@ -1,32 +1,50 @@
-"""Implements functions for performing GET and POST requests."""
+"""Implements GET and POST request methods."""
 import socket
-from collections import namedtuple
 from urllib.parse import urlparse
 
 
 class Response:
-    """"""
+    """Represents an HTTP response message.
+    
+    Attributes:
+        http_version (str): HTTP version.
+        code (int): Status code.
+        status (str): Description of status code.
+        headers (dict): Collection of key value pairs representing the response
+            headers.
+        body (str): The response body.
+    """
 
     def __init__(self, response: str):
-        """"""
+        """Parse the response string."""
         # The first consecutive CRLF sequence demarcates the start of the
         # entity-body.
-        [preamble, self.body] = response.split('\r\n\r\n', maxsplit=1)
-        [status_line, *headers] = preamble.split('\r\n')
-        [self.http_version, code, self.status] = status_line.split()
+        preamble, self.body = response.split("\r\n\r\n", maxsplit=1)
+        status_line, *headers = preamble.split("\r\n")
+        self.http_version, code, *status = status_line.split()
         self.code = int(code)
-        self.headers = dict(kv.split(':') for kv in headers)
+        self.status = " ".join(status)
+        map(_remove_whitespace, headers)
+        self.headers = dict(kv.split(":", maxsplit=1) for kv in headers)
 
     def __str__(self):
-        """"""
-        status_line = '{} {} {}\r\n'.format(self.http_version, self.code,
-                                            self.status)
-        headers = ''.join('{}: {}\r\n'.format(k, v))
+        """Return a string representation of the response."""
+        status_line = "{} {} {}".format(self.http_version, self.code,
+                                        self.status)
+        headers = "\n".join(
+            "{}: {}".format(k, v) for k, v in self.headers.items())
+        return "\n".join((status_line, headers, self.body))
 
 
-# Receives all the data from the socket until EOF or close.
+def _remove_whitespace(s: str):
+    """Return a string with all whitespace removed from the input."""
+    return "".join(s.split())
+
+
 def _recvall(sock: socket.socket):
-    """"""
+    """Receive all data from the socket until EOF or close and return as a byte
+    string.
+    """
     BUFFER_SIZE = 8192
     response = b""
     while True:
@@ -38,126 +56,86 @@ def _recvall(sock: socket.socket):
     return response
 
 
-def _get_body(message: str):
-    """"""
-    # The first consecutive CRLF sequence demarcates the start of the
-    # entity-body.
-    i = message.find('\r\n\r\n')
-    return '' if i == -1 else message[i + 4:]
+def GET(url: str, headers=None):
+    """Perform an HTTP GET request. Return response as a Response object.
 
+    Args:
+        url (str): The URL to perform the GET request on.
+        headers (dict): Dictionary containing key-value pairs representing the
+            request headers. Default is None.
 
-def GET(urlstr: str, headers=None, timeout=None):
-    """TODO docstring.
-    TODO handle timeout exception.
-    TODO check url format?
+    Return:
+        Response: The response from the host.
     """
     if not headers:
         headers = {}
 
     # Extract components from URL. If port is not specified default to 80.
-    url = urlparse(urlstr)
-    netloc = url.netloc
-    host = url.hostname
-    port = url.port if url.port else 80
-    path = url.path if url.path else '\\'
-    query = url.query
-    request_uri = '{}?{}'.format(path, query) if query else path
+    parsed_url = urlparse(url)
+    netloc = parsed_url.netloc
+    host = parsed_url.hostname
+    port = parsed_url.port or 80
+    path = parsed_url.path or "\\"
+    query = parsed_url.query
+    request_uri = "{}?{}".format(path, query) if query else path
 
-    # HTTP 1.1 requires Host header.
-    headers.setdefault('Host', netloc)
-
-    # Open a socket and connect to host, setting a timeout if necessary.
-    socket_address = (host, port)
-    sock = socket.socket(family=socket.AF_INET)
-    if timeout:
-        sock.settimeout(timeout)
-    sock.connect(socket_address)
+    # Add standard headers.
+    headers.setdefault("Host", netloc)
+    headers.setdefault("User-Agent", "Concordia-HTTP/1.0")
 
     # Construct HTTP request message.
-    request_line = 'GET {} HTTP/1.0'.format(request_uri)
-    headers_line = ''.join(
-        '{}:{}\r\n'.format(k, v) for k, v in headers.items())
-    entity_body = ''
-    request = '\r\n'.join((request_line, headers_line, entity_body))
+    request_line = "GET {} HTTP/1.0".format(request_uri)
+    headers_line = "".join(
+        "{}:{}\r\n".format(k, v) for k, v in headers.items())
+    entity_body = ""
+    request = "\r\n".join((request_line, headers_line, entity_body))
 
-    # Send request message to host and retrieve the response message.
-    sock.sendall(request.encode())
-    response = _recvall(sock).decode('UTF-8')
+    # Open a socket, connect to host, send request, and retrieve response.
+    sock = socket.create_connection((host, port))
+    sock.sendall(request.encode("UTF-8"))
+    response = _recvall(sock).decode("UTF-8")
 
     return Response(response)
 
 
-# Send a post request to the given url, with the data and headers.
-# Prints debug information if verbose is set TODO verbose flag
-#
-# The function returns a dictionary with the key headers, body and status
-def POST(url: str, data=None, headers={}):
+def POST(url: str, data="", headers=None):
+    """Perform an HTTP POST request. Return response as a Response object.
+
+    Args:
+        url (str): The URL to perform the GET request on.
+        data (str): Data to sent in request body. Defaults to empty string.
+        headers (dict): Dictionary containing key-value pairs representing the
+            request headers. Default is None.
+
+    Returns:
+        Response: The response from the host.
+    """
+    if not headers:
+        headers = {}
+
+    # Extract components from URL. If port is not specified default to 80.
     parsed_url = urlparse(url)
-    port = parsed_url.port if parsed_url.port else 80
-    method = "POST"
-    http_version = "1.0"
+    netloc = parsed_url.netloc
+    host = parsed_url.hostname
+    port = parsed_url.port or 80
+    path = parsed_url.path or "\\"
+    query = parsed_url.query
+    request_uri = "{}?{}".format(path, query) if query else path
 
-    content_length = len(data) if data else 0
+    # Add standard headers.
+    headers.setdefault("Host", netloc)
+    headers.setdefault("User-Agent", "Concordia-HTTP/1.0")
+    headers.setdefault("Content-Length", str(len(data)))
 
-    # Add Standard headers
-    headers["Host"] = parsed_url.hostname
-    headers["User-Agent"] = "Concordia-HTTP/1.0"
-    headers["Content-Length"] = str(content_length)
+    # Construct HTTP request message.
+    request_line = "POST {} HTTP/1.0".format(request_uri)
+    headers_line = "".join(
+        "{}:{}\r\n".format(k, v) for k, v in headers.items())
+    request = "\r\n".join((request_line, headers_line, data))
 
-    header_txt = build_headers(headers)
-
-    # Build request
-    request = method + " " + parsed_url.path + " HTTP/" + http_version + "\r\n"
-    request += header_txt
-    request += "\r\n"
-
-    if data is not None:
-        request += data
-
-    # Call server
-    sock = socket.create_connection((parsed_url.hostname, port))
-    sock.sendall(request.encode("utf-8"))
-    response = _recvall(sock).decode("utf-8")
+    # Open a socket, connect to host, send request, and retrieve response.
+    sock = socket.create_connection((host, port))
+    sock.sendall(request.encode("UTF-8"))
+    response = _recvall(sock).decode("UTF-8")
 
     return Response(response)
-
-
-# Parse the response string and returns a dict with headers, body, status{code,text}
-def parse_response(response: str):
-    response = response.split("\r\n\r\n")
-    headers = response[0].split("\r\n")
-    body = response[1]
-
-    status = headers[0].split(" ")
-    status_code = int(status[1])
-    status_txt = status[2]
-
-    headers = parse_headers(headers[1:])
-    return {
-        "status": {
-            "code": status_code,
-            "text": status_txt
-        },
-        "headers": headers,
-        "body": body
-    }
-
-
-# Parse a list of headers line into a dict of name : value
-def parse_headers(headers):
-    header_map = {}
-    for header in headers:
-        prs = header.split(":")
-        name = prs[0].strip()
-        val = prs[1].strip()
-        header_map[name] = val
-
-    return header_map
-
-
-# Builds a string of headers from a dict of name : val
-def build_headers(headers):
-    header_text = ""
-    for name, val in headers.items():
-        header_text += name + " : " + val + "\r\n"
-    return header_text
