2df9fe5436918bc8e424d1d25156cc02adc72db7
SomeMatthew <someMathew@outlook.com>
2018-03-18 08:36:43 -0400

    Add Support for Host Header. Added Server header in response.

--- httpfs/__main__.py
+++ httpfs/__main__.py
@@ -4,4 +4,4 @@ from http_fs import HTTPHandlerFs
 
 
 logger.set_logger(True)
-http_server.start_server('', 8081, HTTPHandlerFs)
+http_server.start_server('127.0.0.1', 8080, HTTPHandlerFs)
--- httpfs/http_server.py
+++ httpfs/http_server.py
@@ -6,6 +6,7 @@ from threading import Thread
 from abc import ABC, abstractmethod
 
 http_server_version = 'HTTP/1.0'
+http_server_name = 'PoorServer'
 http_server_CRLF = '\r\n'.encode('UTF-8')
 http_server_CR = '\r'.encode('UTF-8')
 http_server_LF = '\n'.encode('UTF-8')
@@ -75,14 +76,16 @@ class ConnectionHandlerThread(Thread):
             server = HTTPServer(self.conn)
             request_preamble = self._recv_preamble()
             request_data = b''
-            if 'Content-Length' in request_preamble.headers:
-                data_length = request_preamble.headers['Content-Length']
-                request_data = _recvall(self.conn, int(data_length))
+            if 'Host' in request_preamble.headers and request_preamble.headers['Host'] == self.addr[0]:
+                if 'Content-Length' in request_preamble.headers:
+                    data_length = request_preamble.headers['Content-Length']
+                    request_data = _recvall(self.conn, int(data_length))
 
-            request = Request(request_preamble, request_data.decode('UTF-8'))
-
-            http_handler = self.HTTPHandlerImplClass(request, server)
-            http_handler.handle()
+                request = Request(request_preamble, request_data.decode('UTF-8'))
+                http_handler = self.HTTPHandlerImplClass(request, server)
+                http_handler.handle()
+            else:
+                server.error(404, "Not Found")
         except Exception as inst:
             logger(inst)
             server.error(500, 'Internal Server Error')
@@ -97,17 +100,19 @@ class ConnectionHandlerThread(Thread):
             last_line = _recvline(self.conn)
             if http_server_CRLF == last_line or http_server_LF == last_line:
                 break
-            else :
+            else:
                 data += last_line
         return RequestPreamble(data.decode('UTF-8'))
 
 
+## TODO handling response and error
 class HTTPServer:
     def __init__(self, conn):
         self.conn = conn
 
     def error(self, status: int, msg: str):
-        response = "{} {} {}".format(http_server_version, status, msg)
+        response_line = "{} {} {}".format(http_server_version, status, msg)
+        response = "\r\n".join((response_line, "Server: {}".format(http_server_name), "Content-Length: 0"))
         self.conn.sendall(response.encode("UTF-8"))
         self.conn.close()
 
@@ -117,16 +122,16 @@ class HTTPServer:
 
 
 class HTTPHandler(ABC):
-    def __init__(self, request: 'Request'   , server: HTTPServer):
+    def __init__(self, request: 'Request', server: HTTPServer):
         self.request = request
         self.server = server
 
     def handle(self):
         logger.write('handling request\n{}'.format(self.request))
 
-        if self.request.preamble.http_method.casefold() == 'get'.casefold():
+        if self.request.preamble.http_method == 'GET':
             self.do_GET()
-        elif self.request.preamble.http_method.casefold() == 'post'.casefold():
+        elif self.request.preamble.http_method == 'POST':
             self.do_POST()
         else:
             self.do_invalid_method()
@@ -161,7 +166,7 @@ class RequestPreamble:
         preamble = preamble.strip()
         request_line, *headers = preamble.split("\r\n")
         self.http_method, self.url, self.http_version = request_line.split()
-        map(self._remove_whitespace, headers)
+        headers = [self._remove_whitespace(s) for s in headers]
         self.headers = dict(kv.split(":", maxsplit=1) for kv in headers)
 
     @staticmethod
