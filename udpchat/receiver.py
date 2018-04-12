5d1f60ad3adaee97b10292329cbfaaafe9071b7d
Alessandro Power <alessandrojpower@gmail.com>
2018-04-12 14:38:54 -0400

    No longer using HTTP (??) in receiver.

--- udpchat/receiver.py
+++ udpchat/receiver.py
@@ -1,47 +1,16 @@
 """Receiver module for the UDP chat.
 
-This handles accepting incoming connections and extracting the data from them.
+This handles the acceptance of incoming connections and extracting the data sent
+from them.
 """
+import datetime
 import socket
 from udpmessage import Message
 
-_CRLF = '\r\n'.encode('UTF-8')
-_LF = '\n'.encode('UTF-8')
 
-
-def _recvline(sock: socket.socket):
-    """Receive a single line from the socket until EOF, close, or CRLF and
-    return as a byte string.
-    """
-    BUFFER_SIZE = 1
-
-    data = b""
-    while True:
-        last_buff = sock.recv(BUFFER_SIZE)
-        if not last_buff:
-            break
-        data += last_buff
-        if data.endswith(_CRLF):
-            break
-    return data
-
-
-def _recv_preamble(conn: socket.socket):
-    """Receives all the headers from the socket and return a RequestPreamble.
-    """
-    data = b''
-    while True:
-        last_line = _recvline(conn)
-        if last_line == _CRLF or last_line == _LF:
-            break
-        else:
-            data += last_line
-    return RequestPreamble(data.decode('UTF-8'))
-
-
-def _recvall(sock: socket.socket, length: int):
-    """Receive all data from the socket until length, EOF, or close and return
-    as a byte string.
+def _recvall(sock: socket.socket):
+    """Receive all data from the socket until EOF or close and return as a byte
+    string.
     """
     BUFFER_SIZE = 8192
 
@@ -49,21 +18,9 @@ def _recvall(sock: socket.socket, length: int):
     while True:
         last_buff = sock.recv(BUFFER_SIZE)
         data += last_buff
-        if not last_buff or len(data) >= length:
+        if not last_buff:
             break
     return data
-
-
-def _extractData(sock: socket.socket):
-    """Extracts the request data from the connection socket."""
-    requestPreamble = _recv_preamble(sock)
-    requestData = b''
-
-    if 'Content-Length' in requestPreamble.headers:
-        dataLength = int(requestPreamble.headers['Content-Length'])
-        requestData = _recvall(sock, dataLength)
-
-    return requestData
 
 
 def receiver(port: int):
@@ -78,54 +35,8 @@ def receiver(port: int):
 
     while True:
         conn, _ = sock.accept()
-        requestData = _extractData(conn)
-        message = Message.parse(requestData)
-        print(message)
+        data = _recvall(conn)
+        message = Message.parse(data)
 
-
-class RequestPreamble:
-    """Represents the HTTP request line and headers
-
-    Attributes:
-        http_method (str): HTTP Method.
-        http_version (str): HTTP Version.
-        url (str): Request URL
-        headers (dict): Collection of key value pairs representing the request headers.
-    """
-
-    def __init__(self, preamble: str):
-        """Parse the request preamble string."""
-        # The first consecutive CRLF sequence demarcates the start of the
-        # entity-body.
-        preamble = preamble.strip()
-        request_line, *headers = preamble.split("\r\n")
-        self.http_method, self.url, self.http_version = request_line.split()
-        headers = [self._remove_whitespace(s) for s in headers]
-        self.headers = dict(kv.split(":", maxsplit=1) for kv in headers)
-
-    @staticmethod
-    def _remove_whitespace(s: str):
-        """Return a string with all whitespace removed from the input."""
-        return "".join(s.split())
-
-
-class Request:
-    """Represents an HTTP request.
-
-    Attributes:
-        preamble (RequestPreamble): Request preamble with request line and headers
-        body (str): The request body.
-    """
-
-    def __init__(self, preamble: RequestPreamble, body: str):
-        self.preamble = preamble
-        self.body = body
-
-    def __str__(self):
-        """Return a string representation of the request."""
-        request_line = "{} {} {}".format(self.preamble.http_method,
-                                         self.preamble.url,
-                                         self.preamble.http_version)
-        headers = "\n".join(
-            "{}: {}".format(k, v) for k, v in self.preamble.headers.items())
-        return "\n".join((request_line, headers, self.body))
+        dt = datetime.datetime.now()
+        print('{} [{}]: {}'.format(dt, message.user, message.message))
